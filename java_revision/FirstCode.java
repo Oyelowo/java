634c709a18f17d1aa6d293b33ba502049f0a31d1
Oyelowo Oyedayo <oyelowo.oyedayo@relex.fi>
2018-12-26 12:18:09 +0200

    first and second basic challenge

--- java_revision/FirstCode.java
+++ java_revision/FirstCode.java
@@ -3,7 +3,6 @@
  */
 public class FirstCode {
     public static void main(String[] args) {
-        
        for (int i = 1; i < 7; i++) {
            char leta = 'A';
            for (int j = 1; j <= i; j++) {
