ecbe6aae60851ea206602affabf04328e47631bf
Oyelowo Oyedayo <oyelowo.oyedayo@relex.fi>
2018-12-26 23:19:58 +0200

    anonymous class and interface

---
+++ src/AnonymousWithInterface.java
@@ -0,0 +1,26 @@
+
+interface Abc {
+    void show();
+    
+}
+
+/* class Implimentor implements Abc {
+    public void show() {
+        System.out.print("implemnted");
+    }
+    
+} */
+
+public class AnonymousWithInterface {
+
+    public static void main(String[] args) {
+        // This cannot be reused but save memory
+        Abc obj = new Abc(){
+                public void show() {
+                    System.out.print("implemnted anoonymously");
+                }
+        };
+        obj.show();
+    }
+    
+}
