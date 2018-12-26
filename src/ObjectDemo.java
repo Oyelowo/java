6ad37b55b9e2df28da800d0cbd5d7e2c1933f942
Oyelowo Oyedayo <oyelowo.oyedayo@relex.fi>
2018-12-26 13:25:39 +0200

    constructor and method overloading

--- src/ObjectDemo.java
+++ src/ObjectDemo.java
@@ -4,27 +4,54 @@
 /**
  * Class
  */
-class Calc {
+class Casio {
+
     int num1;
     int num2;
-    int result;
-
-    public void perform(){
-        result = num1 + num2;
+    String operation;
+    public  Casio() {
+        num1= 0;
+        num2=0;
+        operation = "Nothing";
     }
+
+    public Casio(int i) {
+        num1 = i;
+        num2 = 0;
+        operation = "Nothing";
+    }
+
+    public Casio(int i, int j, String op) {
+        num1 = i;
+        num2= j;
+        operation = op;
+    }
+
     
+  public void add(int i, int j) {
+      System.out.println(i+j);
+  }
+
+  public void add(double i, double j) {
+    System.out.println(i+j);
 }
+
+  public void add(int i, int j, int k) {
+    System.out.println(i+j + k);
+}  
+}
+
+
 
 public class ObjectDemo {
 
     public static void main(String[] args) {
-        Calc obj = new Calc();   //knows and does something
-        obj.num1 = 3;
-        obj.num2 = 6;
+        Casio myCalc = new Casio(4, 5, "Add");
+        myCalc.add(300, 5);
 
-        obj.perform();
+        myCalc.add(78, 56, 767);
 
-        System.out.println(obj.result);
+        myCalc.add(300.656, 5.354);
         
     }
 }
