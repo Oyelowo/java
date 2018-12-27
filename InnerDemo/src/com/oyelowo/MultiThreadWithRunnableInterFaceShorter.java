b8ff7819532a0bf4d24bbf3c02a8aa46463d6d8d
Oyelowo Oyedayo <oyelowo.oyedayo@relex.fi>
2018-12-28 00:37:23 +0200

    Thread priority

--- InnerDemo/src/com/oyelowo/MultiThreadWithRunnableInterFaceShorter.java
+++ InnerDemo/src/com/oyelowo/MultiThreadWithRunnableInterFaceShorter.java
@@ -1,19 +1,18 @@
 package com.oyelowo;
 
 
-
 public class MultiThreadWithRunnableInterFaceShorter {
-    public static void main(String[] args) throws Exception{
-        Thread t1 = new Thread(() ->{
+    public static void main(String[] args) throws Exception {
+        Thread t1 = new Thread(() -> {
             for (int i = 1; i <= 5; i++) {
-                System.out.println("Hi");
+                System.out.println("Hi " + Thread.currentThread().getPriority());
                 try {
                     Thread.sleep(1000);
                 } catch (Exception e) {
                 }
                 ;
             }
-        });
+        }, "First Thread");
         Thread t2 = new Thread(() -> {
             for (int i = 1; i <= 5; i++) {
                 System.out.println("Hello");
@@ -23,7 +22,23 @@ public class MultiThreadWithRunnableInterFaceShorter {
                 }
                 ;
             }
-        });
+        }, "Second Thread");
+
+        // Another way to set thread names
+        //t1.setName("First Thread");
+        //t2.setName("Second Thread");
+        //System.out.println(t1.getName());
+        //System.out.println(t2.getName());
+
+        //t1.setPriority(1);
+        //t2.setPriority(10);
+
+        t1.setPriority(Thread.MIN_PRIORITY);
+        t2.setPriority(Thread.MAX_PRIORITY);
+
+        System.out.println(t1.getPriority());
+        System.out.println(t2.getPriority());
+
 
         t1.start();
         try {
@@ -33,12 +48,11 @@ public class MultiThreadWithRunnableInterFaceShorter {
         ;
         t2.start();
 
-
         System.out.println(t1.isAlive());
 
         t1.join();
         t2.join();
-        
+
         System.out.println(t1.isAlive());
         System.out.println("Bye");
     }
